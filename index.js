0de61284527c3bc9655582f0335f50c28ad98f9a
Ryan Kirkland <ryan.j.kirkland86@gmail.com>
2021-01-19 17:55:17 -0800

    Adding user prompts

--- index.js
+++ index.js
@@ -3,3 +3,26 @@ const util = require("util");
 const inquirer = require("inquirer");
 const writeFileAsync = util.promisify(fs.writeFile);
 
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: "What is your GitHub username?",
+            name: 'username',
+        },
+        {
+            type: 'input',
+            message: "What is your email address?",
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: "What is your project's name?",
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: "Please write a short description of your project?",
+            name: 'description',
+        }
+    ])
