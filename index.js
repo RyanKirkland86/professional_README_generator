c178d32f1d3d3badf6178bd9d6aa84813660fc81
Ryan Kirkland <ryan.j.kirkland86@gmail.com>
2021-01-19 18:07:38 -0800

    Adding more user prompts to index.js file

--- index.js
+++ index.js
@@ -24,5 +24,31 @@ inquirer
             type: 'input',
             message: "Please write a short description of your project?",
             name: 'description',
+        },
+        {
+            type: 'list',
+            message: "What kind of license does your project use?",
+            name: 'license',
+            choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+        },
+        {
+            type: 'input',
+            message: "What command should be run to install dependencies?",
+            name: 'dependencies',
+        },
+        {
+            type: 'input',
+            message: "What command should be run to run tests?",
+            name: 'tests',
+        },
+        {
+            type: 'input',
+            message: "What does the user need to know about using the repo?",
+            name: 'repo',
+        },
+        {
+            type: 'input',
+            message: "What does the user need to know about contributing to the repo?",
+            name: 'contributing',
         }
     ])
